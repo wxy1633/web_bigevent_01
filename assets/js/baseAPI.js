e66d8635914758b208d3455c57ca3fa4954bf48f
wxy <1633910783@qq.com>
2020-11-26 10:29:30 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,7 +4,31 @@ var baseURL = 'http://ajax.frontend.itheima.net'
 // 生产环境
 
 // 拦截所有ajax请求： get/post/ajax
-$.ajaxPrefilter(function(params) { 
-   // 拼接对应环境的服务器地址
-     params.url = baseURL + params.url;
-  });
+$.ajaxPrefilter(function (params) {
+  // 拼接对应环境的服务器地址
+  params.url = baseURL + params.url;
+
+  // 对需要权限的接口配置头信息
+  // 必须以my开头才行
+  if (params.url.indexOf('/my/') !== -1) {
+    params.headers = {
+      // 访问后台页面需要  token
+      // 重新登录，因为 token过期事件12小时
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+  // 3 拦截所有响应，判断身份验证
+  params.complete = function (res) {
+    console.log(res.responseJSON);
+    var obj = res.responseJSON
+    console.log(obj);
+    if (obj.status == 1 && obj.message == '身份认证失败！') {
+      // 1.清空本地token
+      localStorage.removeItem('token')
+      // 2.页面跳转
+      location.href = '/login.html'
+    }
+  }
+
+  
+});
---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+$(function () {
+    // 1.获取用户信息
+    getUserInfo()
+    // 2.退出按钮
+    $('#btnLogout').on('click',function () {
+        
+        layer.confirm('是否确认退出？', {icon:3,title:'提示'},function (index) {
+            // 1.清空本地token 
+            localStorage.removeItem('token')
+            // 2.页面跳转
+            location.href = '/login.html'
+            // 3.关闭询问框
+            layer.close(index)
+        })  
+           
+    })
+})
+
+// 2获取用户信息 函数（封装到入口函数的外面了）
+// 原因：后面其他的页面要调用
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     // 访问后台页面需要  token
+        //     // 重新登录，因为 token过期事件12小时
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) return layui.layer.msg(res.message)
+            // 请求成功，渲染用户头像信息
+            rendedAvatar(res.data)
+        }
+
+
+    })
+}
+
+// 3封装用户头像函数
+function rendedAvatar(user) {
+    // 1.用户名
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 2.用户头像
+    if(user.user_pic !== null){
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    }else{
+        $('.layui-nav-img').hide()
+        var text = name[0].toUpperCase()
+        $('.text-avatar').html(text).show()
+    }
+}
